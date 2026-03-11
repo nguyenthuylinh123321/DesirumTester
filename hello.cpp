12d46937766aa2eb1d8a1f2bc830f16e680d4d1c
butterflyepuck <levanlinhepi@gmail.com>
2026-03-11 18:04:58 +0700

    34344

---
+++ hello.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Admin on 3/11/2026.
+//
+
+#include "hello.h"
---
+++ hello.h
@@ -0,0 +1,14 @@
+//
+// Created by Admin on 3/11/2026.
+//
+
+#ifndef DESIRUMTESTER_HELLO_H
+#define DESIRUMTESTER_HELLO_H
+
+
+class hello
+{
+};
+
+
+#endif //DESIRUMTESTER_HELLO_H
